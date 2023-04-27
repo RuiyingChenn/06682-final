6e480d1190a2393ada9440482dce4912cf7d3e64
RuiyingChenn <ruiyingc@andrew.cmu.edu>
2023-04-27 20:15:54 +0000

    modify description in setup.py

--- package/setup.py
+++ package/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name="s23openalex",
     version="0.0.1",
-    description="provides openalex work class and other utilities",
+    description="Openalex utilities",
     maintainer="Ruiying Chen",
     maintainer_email="ruiyingc@andrew.cmu.edu",
     license="MIT",
